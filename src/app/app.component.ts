5b6bc47a4424cd74283a81a40f0251fe85765147
Austin Roberts and Kristi Hwang <kristi@>
2018-06-04 16:50:13 -0700

    update todo with Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'look-at-me-Im-a-selector',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  currentFocus: string ='Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  firstTask: Task = new Task("Finish weekend Angular homework for Epicodus course");
 }
+// export class Task {
+//   public done: boolean = false;
+//   constructor(public description: string) { }
+// }
